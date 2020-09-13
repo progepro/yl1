cfc0d63b610b81c2e1913f22cf6861e9896341aa
progepro <keijo.kolga@gmail.com>
2020-09-13 17:38:08 +0300

    Initial commit

---
+++ src/Yl1.java
@@ -0,0 +1,12 @@
+/**
+ * @author Keijo Kolga 193255 EDTR23
+ */
+public class Yl1 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 28;
+        int summa = a + b;
+
+        System.out.println(summa);
+    }
+}
